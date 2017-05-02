bab0480faa8fbbb5feb9ae43e4308df7d64a4019
Chaz Henricks <chazhenricks@gmail.com>
2017-05-02 14:25:57 -0500

    IM WORKING ON IT OK

---
+++ js/modal-color.js
@@ -0,0 +1,15 @@
+console.log("COLOR SELECT RICKK!!!!!");
+
+
+var Chatty = (function(chatapp){
+
+    var colorBtn = document.getElementById("color-btn");
+
+    chatapp.setUserColor = function(){
+
+    }
+
+
+    return chatapp;
+
+})(Chatty || {});
